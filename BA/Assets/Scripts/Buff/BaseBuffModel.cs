3d624ff0a3d3c5b1dd192b4da00acff95a5f653d
Slime862 <2058456435@qq.com>
2024-03-10 22:56:56 +0800

    1

--- BA/Assets/Scripts/Buff/BaseBuffModel.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BaseBuffModel : ScriptableObject
-{
-    public abstract void Apply(BuffInfo buffInfo,DamageInfo damageInfo=null);
-}
